70f666a5ab08d42ec0221166cbecb292febba262
Julibio <julibio@hotmail.com>
2022-06-30 13:51:50 -0300

    descrição do commit

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Curso, Diario
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Curso)
+admin.site.register(Diario)
